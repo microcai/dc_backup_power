a51de7bc18c5e9000fb6e1a38ea55f96a9c1020a
microcai <microcaicai@gmail.com>
2023-10-20 02:26:35 +0800

    add TODO

--- include/controller.hpp
+++ include/controller.hpp
@@ -22,11 +22,30 @@ public:
     void loop();
 
 protected:
-    void setPWM(float duty);
+    void charge_loop();
+
+    void output_loop();
+
+    void setPWM(float duty, bool enable_pwm = true);
 
 private:
 
+    enum
+    {
+        // 充电模式.
+        CHARGE_MODE,
+        // 输出模式.
+        OUTPUT_MODE,
+
+        // 电池保护模式.
+        PROTECT_MODE,
+    } mode = PROTECT_MODE;
+
     INA226 m_ina_dc;
     INA226 m_ina_bat;
+
+public:
+    float m_min_dc_voltage = 11;
+    float m_min_bat_voltage = 3.3;
 
 };
--- src/controller.cpp
+++ src/controller.cpp
@@ -2,12 +2,27 @@
 
 
 #include "controller.hpp"
-
+#include "ArduinoNvs.h"
 
 Controller::Controller()
     : m_ina_dc(0x41)
     , m_ina_bat(0x40)
 {
+  NVS.begin();
+
+  if (NVS.getInt("magic") == 7788)
+  {
+    NVS.getFloat("bat_protect_voltage");
+    NVS.getFloat("bat_min_voltage");
+    NVS.getFloat("bat_max_voltage");
+
+    NVS.getFloat("dc_min_voltage");
+  }
+  else
+  {
+
+  }
+
   Wire.setPins(19,18);
 
   // 10mΩ shunt
@@ -29,10 +44,67 @@ Controller::Controller()
 
 void Controller::loop()
 {
+  switch (mode)
+  {
+    case CHARGE_MODE:
+    {
+      charge_loop();
+      // detect work mode.
+      if ( m_ina_dc.getBusVoltage() < m_min_dc_voltage)
+      {
+        mode = OUTPUT_MODE;
+      }
 
+    }
+    break;
+    case OUTPUT_MODE:
+    {
+      output_loop();
+
+      if (m_ina_bat.getBusVoltage() < m_min_bat_voltage )
+      {
+        mode = PROTECT_MODE;
+      }
+    }break;
+    case PROTECT_MODE:
+    {
+      // 等待 dc 电力恢复.
+      if (m_ina_dc.getBusVoltage() > (m_ina_bat.getBusVoltage() + 0.1))
+      {
+        // 有电就充一会
+        charge_loop();
+      }
+      else
+      {
+        // 关闭输出
+        setPWM(0, false);
+      }
+
+      if (m_ina_bat.getBusVoltage() > m_min_bat_voltage)
+      {
+        mode = CHARGE_MODE;
+      }
+    }
+  }
 }
 
-void Controller::setPWM(float duty)
+void Controller::setPWM(float duty, bool enable_pwm)
 {
-    ledcWrite(0, duty * (2<<pwm_resolution_bits - 1));
+  ledcWrite(0, duty * (2<<pwm_resolution_bits - 1));
+  if (enable_pwm)
+    digitalWrite(PWM_EN, 1);
+  else
+    digitalWrite(PWM_EN, 0);
+}
+
+void Controller::charge_loop()
+{
+  // TODO
+  // 使用 3p3z 算法确定 pwm 占空比。使得充电电流不超过设定值. 同时充电电压也不超过设定值.
+}
+
+void Controller::output_loop()
+{
+  // TODO
+  // 使用 3p3z 算法确定 pwm 占空比，使得输出电压不超过设定值.
 }
